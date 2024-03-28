eeb6d2c43ed1af7f52817921f57370b91203deef
Simonxu <q2798573282.gmail.com>
2024-03-28 16:13:23 +0800

    add more functions valueiterationwithgpu and args.gpu

--- version0/script/HW.py
+++ version0/script/HW.py
@@ -8,10 +8,12 @@ import argparse
 import logging
 import function_board as fb
 import function_tool as ft
+import cProfile
 import function_get_aiming_grid
-from function_solve_dp_withturn import solve_dp_withturn_valueiteration, setup_logging, log_system_info
+from function_solve_dp_withturn import solve_dp_withturn_valueiteration, setup_logging, log_system_info, \
+    solve_dp_withturn_policyiteration, solve_dp_withturn_valueiteration_gpu
 
-#  python HW.py --playerID_list 7 11 12 --iter_error=0.00001 --iter_limit=5000 --method='policy'
+#  python HW.py --playerID_list 7 --iter_error=0.0001 --iter_limit=5000 --method='value'
 np.set_printoptions(precision=4)
 np.set_printoptions(linewidth=300)
 np.set_printoptions(threshold=300)
@@ -21,34 +23,41 @@ parser.add_argument('--iter_limit', type=int, help='Iteration limit for the valu
 parser.add_argument('--iter_error', type=float, help='Error threshold for the value iteration algorithm',
                     default=0.0001)
 parser.add_argument('--method', type=str, help='algorithm type', choices=['value', 'policy'], required=True)
+parser.add_argument('--gpu', action='store_true', help='Use GPU for training')
 
 args = parser.parse_args()
 iter_limit = args.iter_limit
 iter_error = args.iter_error
 playerID_list = args.playerID_list
 method = args.method
-
+gpu = args.gpu
+function_map = {
+    ('value', True): solve_dp_withturn_valueiteration_gpu,
+    ('value', False): solve_dp_withturn_valueiteration,
+    ('policy', True): solve_dp_withturn_policyiteration,
+    ('policy', False): solve_dp_withturn_policyiteration,
+}
 result_dir = '../HW_result'
 if not os.path.isdir(result_dir):
     os.makedirs(result_dir)
 
 for playerID in playerID_list:
-    logger = setup_logging(result_dir, playerID)
+    logger = setup_logging(method, result_dir, playerID)
     log_system_info(logger)
     name_pa = 'player{}'.format(playerID)
     [aiming_grid, prob_grid_normalscore, prob_grid_singlescore, prob_grid_doublescore, prob_grid_triplescore,
      prob_grid_bullscore] = function_get_aiming_grid.load_aiming_grid(name_pa)
 
     t1 = time.time()
-    V, optimal_action_index = solve_dp_withturn_valueiteration(aiming_grid, prob_grid_normalscore,
-                                                               prob_grid_doublescore,
-                                                               prob_grid_bullscore, iter_limit=iter_limit,
-                                                               iter_error=iter_error, logger=logger)
+    V, optimal_action_index = function_map[(method, gpu)](aiming_grid, prob_grid_normalscore,
+                                                          prob_grid_doublescore,
+                                                          prob_grid_bullscore, iter_limit=iter_limit,
+                                                          iter_error=iter_error, logger=logger)
     t2 = time.time()
 
     print(f'\nsolve dp_withturn in {t2 - t1} seconds for player{playerID}')
-    np.save(os.path.join(result_dir, f'player{playerID}_V.npy'), V)
-    np.save(os.path.join(result_dir, f'player{playerID}_optimal_action_index.npy'), optimal_action_index)
+    np.save(os.path.join(result_dir, f'player{playerID}_valueiter_V.npy'), V)
+    np.save(os.path.join(result_dir, f'player{playerID}_valueiter_Mu.npy'), optimal_action_index)
     logger.info(f'Saved V matrix and optimal action index for player {playerID}')
     logger.info(f'Total time for value iteration: {t2 - t1:.4f} seconds.')
     print('\n')
--- version0/script/function_solve_dp_withturn.py
+++ version0/script/function_solve_dp_withturn.py
@@ -5,13 +5,13 @@ import time
 import os
 import logging
 import platform
+import psutil
 
-
-def setup_logging(result_dir, playerID):
-    logger = logging.getLogger(f'ValueIterationLogger_{playerID}')
+def setup_logging(method, result_dir, playerID):
+    logger = logging.getLogger(f'{method}IterationLogger_{playerID}')
     logger.setLevel(logging.DEBUG)
 
-    log_file = os.path.join(result_dir, f'player{playerID}_value_iteration.log')
+    log_file = os.path.join(result_dir, f'player{playerID}_{method}_iteration.log')
     file_handler = logging.FileHandler(log_file, mode='a')
     file_handler.setLevel(logging.DEBUG)
 
@@ -28,10 +28,15 @@ def log_system_info(logger):
     logger.info(f"Operating System: {platform.system()} {platform.release()}")
     logger.info(f"Machine: {platform.machine()}")
     logger.info(f"Processor: {platform.processor()}")
+    cpu_info = psutil.cpu_freq()
+    logger.info(f"CPU Cores: {psutil.cpu_count(logical=True)}")
+    logger.info(f"CPU Frequency: {cpu_info.current:.2f} MHz (Min: {cpu_info.min:.2f} MHz, Max: {cpu_info.max:.2f} MHz)")
+    svmem = psutil.virtual_memory()
+    logger.info(f"Total Memory: {svmem.total / (1024.0 ** 3):.2f} GB")
 
 
 def solve_dp_withturn_valueiteration(aiming_grid, prob_grid_normalscore, prob_grid_doublescore=None,
-                                     prob_grid_bullscore=None,iter_limit=1000, iter_error=0.0001, logger=None):
+                                     prob_grid_bullscore=None, iter_limit=1000, iter_error=0.0001, logger=None):
     num_aiming_location = aiming_grid.shape[0]
     prob_normalscore_0tosmax_dic = {}
     prob_normalscore_0tosmaxsum_dic = {}
@@ -137,3 +142,222 @@ def solve_dp_withturn_valueiteration(aiming_grid, prob_grid_normalscore, prob_gr
         print(f"\rscore_state = {s} finished.", end="")
 
     return V, optimal_action_index
+
+
+def solve_dp_withturn_policyiteration(aiming_grid, prob_grid_normalscore, prob_grid_doublescore=None,
+                                      prob_grid_bullscore=None, iter_limit=1000, iter_error=0.0001, logger=None):
+    num_aiming_location = aiming_grid.shape[0]
+    prob_normalscore_0tosmax_dic = {}
+    prob_normalscore_0tosmaxsum_dic = {}
+    for score_max in range(0, 61):
+        score_max_plus1 = score_max + 1
+        prob_normalscore_0tosmax_dic[score_max] = np.array(prob_grid_normalscore[:, 0:score_max_plus1])
+        prob_normalscore_0tosmaxsum_dic[score_max] = prob_normalscore_0tosmax_dic[score_max].sum(axis=1)
+
+    prob_doublescore_dic = {}
+    for doublescore_index in range(20):
+        doublescore = 2 * (doublescore_index + 1)
+        prob_doublescore_dic[doublescore] = np.array(prob_grid_doublescore[:, doublescore_index])
+    prob_doublescore_dic[fb.score_DB] = np.array(prob_grid_bullscore[:, 1])
+
+    # Initialize the value function for all states (s, i, u)
+    V = np.full((502, 3, 121), fill_value=np.nan, dtype=np.float32)
+    optimal_action_index = np.full((502, 3, 121), fill_value=-1, dtype=np.int16)
+    V[0, :, 0] = 0  # If the score is 0, the game is finished
+    V[1, :, 0] = np.inf
+    optimal_action_index[0:2, :, 0] = -999
+
+    for s in range(2, 502):
+        start_time = time.time()
+        values_old = {3: np.ones(min(s - 2, 60 * 0) + 1, dtype=np.float32),
+                      2: np.ones(min(s - 2, 60 * 1) + 1, dtype=np.float32),
+                      1: np.ones(min(s - 2, 60 * 2) + 1, dtype=np.float32)}
+        values = {3: np.ones(min(s - 2, 60 * 0) + 1, dtype=np.float32),
+                  2: np.ones(min(s - 2, 60 * 1) + 1, dtype=np.float32),
+                  1: np.ones(min(s - 2, 60 * 2) + 1, dtype=np.float32)}
+        # values_old = np.ones((4, min(s - 2, 60 * 2) + 1))
+        # values = np.ones((4, min(s - 2, 60 * 2) + 1))
+
+        iter_index = -1
+        for iter_index in range(iter_limit):
+            for u in [0]:
+                score_max = min(s - 2, 60)
+                prob_bust = 1 - prob_normalscore_0tosmaxsum_dic.get(score_max) - prob_doublescore_dic.get(s - u,
+                                                                                                          np.zeros(
+                                                                                                              num_aiming_location, ))  # (984,)
+                turn_to_thrownew = (prob_normalscore_0tosmax_dic.get(score_max).dot(values[2][u:u + score_max + 1])
+                                    + prob_doublescore_dic.get(s, np.zeros(num_aiming_location, )) * 1
+                                    + prob_bust.dot(1 + values[3][0]))
+                values[3][u] = np.min(turn_to_thrownew)
+                optimal_action_index[s, 2, u] = np.argmin(turn_to_thrownew)
+            for u in range(0, min(s - 2, 60) + 1):
+                score_max = min(s - u - 2, 60)
+                prob_bust = 1 - prob_normalscore_0tosmaxsum_dic.get(score_max) - prob_doublescore_dic.get(s - u,
+                                                                                                          np.zeros(
+                                                                                                              num_aiming_location, ))  # (984,)
+                turn_to_thrownew = (
+                        prob_normalscore_0tosmax_dic.get(score_max).dot(values[1][u:u + score_max + 1])
+                        + prob_doublescore_dic.get(s - u, np.zeros(num_aiming_location, )) * 1
+                        + prob_bust.dot(1 + values[3][0])
+                )
+                values[2][u] = np.min(turn_to_thrownew)
+                optimal_action_index[s, 1, u] = np.argmin(turn_to_thrownew)
+            for u in range(1, min(s - 2, 120) + 1):
+                score_max = min(s - u - 2, 60)
+                prob_bust = 1 - prob_normalscore_0tosmaxsum_dic.get(score_max) - prob_doublescore_dic.get(s - u,
+                                                                                                          np.zeros(
+                                                                                                              num_aiming_location, ))  # (984,)
+                turn_to_thrownew = (
+                        prob_normalscore_0tosmax_dic.get(score_max).dot(1 + V[s - u:s - u - score_max - 1:-1, 2, 0])
+                        + prob_doublescore_dic.get(s - u, np.zeros(num_aiming_location, )) * 1
+                        + prob_bust.dot(1 + values[3][0])
+                )
+                values[1][u] = np.min(turn_to_thrownew)
+                optimal_action_index[s, 0, u] = np.argmin(turn_to_thrownew)
+
+            for u in [0]:
+                score_max = min(s - u - 2, 60)
+                prob_bust = 1 - prob_normalscore_0tosmaxsum_dic.get(score_max) - prob_doublescore_dic.get(s - u,
+                                                                                                          np.zeros(
+                                                                                                              num_aiming_location, ))  # (984,)
+                turn_to_thrownew = (
+                        prob_grid_normalscore[:, 0].dot(1 + values[3][0])
+                        + np.array(prob_grid_normalscore[:, 1:score_max + 1]).dot(
+                    1 + V[s - u - 1:s - u - score_max - 1:-1, 2, 0])
+                        + prob_doublescore_dic.get(s - u, np.zeros(num_aiming_location, )) * 1
+                        + prob_bust.dot(1 + values[3][0])
+                )
+                values[1][u] = np.min(turn_to_thrownew)
+                optimal_action_index[s, 0, u] = np.argmin(turn_to_thrownew)
+
+            jump_out_of_iter = True
+            for remaining_throws in [1, 2, 3]:
+                if not np.all(np.abs(values_old[remaining_throws] - values[remaining_throws]) < iter_error):
+                    jump_out_of_iter = False
+                    break
+            if not jump_out_of_iter:
+                values_old = copy.deepcopy(values)
+            else:
+                break
+
+        V[s, 2, 0] = values[3][0]
+        V[s, 1, :len(values[2])] = values[2]
+        V[s, 0, :len(values[1])] = values[1]
+        end_time = time.time()
+        if logger:
+            logger.debug(
+                f"Score state {s} completed in {end_time - start_time:.4f} seconds with {iter_index} iterations.")
+
+        print(f"\rscore_state = {s} finished.", end="")
+
+    return V, optimal_action_index
+
+
+def solve_dp_withturn_valueiteration_gpu(aiming_grid, prob_grid_normalscore, prob_grid_doublescore=None,
+                                     prob_grid_bullscore=None, iter_limit=1000, iter_error=0.0001, logger=None):
+    import cupy as cp
+    num_aiming_location = aiming_grid.shape[0]
+    prob_normalscore_0tosmax_dic = {}
+    prob_normalscore_0tosmaxsum_dic = {}
+    for score_max in range(0, 61):
+        score_max_plus1 = score_max + 1
+        prob_normalscore_0tosmax_dic[score_max] = cp.array(prob_grid_normalscore[:, 0:score_max_plus1])
+        prob_normalscore_0tosmaxsum_dic[score_max] = prob_normalscore_0tosmax_dic[score_max].sum(axis=1)
+
+    prob_doublescore_dic = {}
+    for doublescore_index in range(20):
+        doublescore = 2 * (doublescore_index + 1)
+        prob_doublescore_dic[doublescore] = cp.array(prob_grid_doublescore[:, doublescore_index])
+    prob_doublescore_dic[fb.score_DB] = cp.array(prob_grid_bullscore[:, 1])
+
+    # Initialize the value function for all states (s, i, u)
+    V = cp.full((502, 3, 121), fill_value=cp.nan, dtype=cp.float32)
+    optimal_action_index = cp.full((502, 3, 121), fill_value=-1, dtype=cp.int16)
+    V[0, :, 0] = 0  # If the score is 0, the game is finished
+    V[1, :, 0] = cp.inf
+    optimal_action_index[0:2, :, 0] = -999
+
+    for s in range(2, 502):
+        start_time = time.time()
+        values_old = {3: cp.ones(min(s - 2, 60 * 0) + 1, dtype=cp.float32),
+                      2: cp.ones(min(s - 2, 60 * 1) + 1, dtype=cp.float32),
+                      1: cp.ones(min(s - 2, 60 * 2) + 1, dtype=cp.float32)}
+        values = {3: cp.ones(min(s - 2, 60 * 0) + 1, dtype=cp.float32),
+                  2: cp.ones(min(s - 2, 60 * 1) + 1, dtype=cp.float32),
+                  1: cp.ones(min(s - 2, 60 * 2) + 1, dtype=cp.float32)}
+        # values_old = cp.ones((4, min(s - 2, 60 * 2) + 1))
+        # values = cp.ones((4, min(s - 2, 60 * 2) + 1))
+
+        iter_index = -1
+        for iter_index in range(iter_limit):
+            for u in [0]:
+                score_max = min(s - 2, 60)
+                prob_bust = 1 - prob_normalscore_0tosmaxsum_dic.get(score_max) - prob_doublescore_dic.get(s - u,
+                                                                                                          cp.zeros(
+                                                                                                              num_aiming_location, ))  # (984,)
+                turn_to_thrownew = (prob_normalscore_0tosmax_dic.get(score_max).dot(values[2][u:u + score_max + 1])
+                                    + prob_doublescore_dic.get(s, cp.zeros(num_aiming_location, )) * 1
+                                    + prob_bust.dot(1 + values[3][0]))
+                values[3][u] = cp.min(turn_to_thrownew)
+                optimal_action_index[s, 2, u] = cp.argmin(turn_to_thrownew)
+            for u in range(0, min(s - 2, 60) + 1):
+                score_max = min(s - u - 2, 60)
+                prob_bust = 1 - prob_normalscore_0tosmaxsum_dic.get(score_max) - prob_doublescore_dic.get(s - u,
+                                                                                                          cp.zeros(
+                                                                                                              num_aiming_location, ))  # (984,)
+                turn_to_thrownew = (
+                        prob_normalscore_0tosmax_dic.get(score_max).dot(values[1][u:u + score_max + 1])
+                        + prob_doublescore_dic.get(s - u, cp.zeros(num_aiming_location, )) * 1
+                        + prob_bust.dot(1 + values[3][0])
+                )
+                values[2][u] = cp.min(turn_to_thrownew)
+                optimal_action_index[s, 1, u] = cp.argmin(turn_to_thrownew)
+            for u in range(1, min(s - 2, 120) + 1):
+                score_max = min(s - u - 2, 60)
+                prob_bust = 1 - prob_normalscore_0tosmaxsum_dic.get(score_max) - prob_doublescore_dic.get(s - u,
+                                                                                                          cp.zeros(
+                                                                                                              num_aiming_location, ))  # (984,)
+                turn_to_thrownew = (
+                        prob_normalscore_0tosmax_dic.get(score_max).dot(1 + V[s - u:s - u - score_max - 1:-1, 2, 0])
+                        + prob_doublescore_dic.get(s - u, cp.zeros(num_aiming_location, )) * 1
+                        + prob_bust.dot(1 + values[3][0])
+                )
+                values[1][u] = cp.min(turn_to_thrownew)
+                optimal_action_index[s, 0, u] = cp.argmin(turn_to_thrownew)
+
+            for u in [0]:
+                score_max = min(s - u - 2, 60)
+                prob_bust = 1 - prob_normalscore_0tosmaxsum_dic.get(score_max) - prob_doublescore_dic.get(s - u,
+                                                                                                          cp.zeros(
+                                                                                                              num_aiming_location, ))  # (984,)
+                turn_to_thrownew = (
+                        prob_grid_normalscore[:, 0].dot(1 + values[3][0])
+                        + cp.array(prob_grid_normalscore[:, 1:score_max + 1]).dot(
+                    1 + V[s - u - 1:s - u - score_max - 1:-1, 2, 0])
+                        + prob_doublescore_dic.get(s - u, cp.zeros(num_aiming_location, )) * 1
+                        + prob_bust.dot(1 + values[3][0])
+                )
+                values[1][u] = cp.min(turn_to_thrownew)
+                optimal_action_index[s, 0, u] = cp.argmin(turn_to_thrownew)
+
+            jump_out_of_iter = True
+            for remaining_throws in [1, 2, 3]:
+                if not cp.all(cp.abs(values_old[remaining_throws] - values[remaining_throws]) < iter_error):
+                    jump_out_of_iter = False
+                    break
+            if not jump_out_of_iter:
+                values_old = copy.deepcopy(values)
+            else:
+                break
+
+        V[s, 2, 0] = values[3][0]
+        V[s, 1, :len(values[2])] = values[2]
+        V[s, 0, :len(values[1])] = values[1]
+        end_time = time.time()
+        if logger:
+            logger.debug(
+                f"Score state {s} completed in {end_time - start_time:.4f} seconds with {iter_index} iterations.")
+
+        print(f"\rscore_state = {s} finished.", end="")
+
+    return cp.asnumpy(V), cp.asnumpy(optimal_action_index)
